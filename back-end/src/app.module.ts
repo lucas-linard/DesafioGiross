b637ca0fa86f4434ffbd5072095ef1c226e13daf
LucasLinard <lucas.linard@outlook.com>
2023-11-22 10:26:29 -0300

    build: Import other modules to app.module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MapsModule } from './maps/maps.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [MapsModule,AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
