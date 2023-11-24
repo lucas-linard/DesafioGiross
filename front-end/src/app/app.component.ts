126843c56dae3a4ebc1d43cbfd0e0c5be86b3589
LucasLinard <lucas.linard@outlook.com>
2023-11-24 00:03:58 -0300

    refactor: app-root routing

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,63 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
 
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { Form, FormBuilder, FormsModule, ReactiveFormsModule  } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ZipCodesService } from './services/zip-codes.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    HttpClientModule,
-    MatTabsModule,
-    FormsModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-  ],
-  providers: [ZipCodesService],
+  imports: [CommonModule, RouterOutlet, RouterModule, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  users: any[] = [];
-  constructor(
-    private formBuilder: FormBuilder,
-    private apiService: ZipCodesService
-    ) {}
-
-  async ngOnInit(): Promise<void> {
-    
-  }
-
-  
-
-  formValue : any;
-  payload = this.formBuilder.group({
-    zipCode: [''],
-    radius: [''],
-  })
-
-  async getInput() {
-    let apiUrl = 'http://localhost:3000/maps/geocode?zipCode='+ this.payload.value.zipCode;
-    let api =  await this.apiService.getZipCodes(apiUrl);
-  
-    api.subscribe((data) => {
-      this.users = data as any[];
-      console.log(this.users);
-    }
-    );
-
-    console.log(this.payload.value)
-   }
-
+export class AppComponent {
   title = 'front-end';
 }
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/Home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+];
