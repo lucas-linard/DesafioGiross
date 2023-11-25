056def7607935f835cdd78f1c7fbb8d432caf5e7
LucasLinard <lucas.linard@outlook.com>
2023-11-24 22:47:18 -0300

    Refactor:  getZipCodes method to getSearchNearby

--- front-end/src/app/services/zip-codes.service.ts
+++ front-end/src/app/services/zip-codes.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class ZipCodesService {
   constructor(private http: HttpClient) {}
 
-  async getZipCodes(url: string) {
-    return this.http.get(`http://localhost:3000/${url}`);
+  async getSearchNearby(data: { zip: string; radius: number }) {
+    let key =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJuYW1lIjoibWFyaWEiLCJpYXQiOjE3MDA4NzMyODAsImV4cCI6MTcwMDk1OTY4MH0.-pIoX2Qrjl5kKCWNrSXRHZgYreQIcNW3iiRgxv3jn5w';
+    return this.http.get(
+      `http://localhost:3000/maps/searchNearby?zip=${data.zip}&radius=${
+        data.radius * 1000
+      }`,
+      { headers: { Authorization: `Bearer ${key}` } }
+      // }`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+    );
   }
 }
