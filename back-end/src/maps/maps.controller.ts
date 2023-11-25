e75f9759c229956222644400c29c23cb1db7fa9f
LucasLinard <lucas.linard@outlook.com>
2023-11-24 22:46:06 -0300

    Refactor: maps controller to use authentication and optimize geocode retrieval

--- back-end/src/maps/maps.controller.ts
+++ back-end/src/maps/maps.controller.ts
@@ -1,23 +1,24 @@
-import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { MapsService } from './maps.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('maps')
 export class MapsController {
   constructor(private readonly mapsService: MapsService) {}
 
-  @Get('geocode')
-  getGeocode(@Request() req): Promise<any> {
-    return this.mapsService.getGeocode(req.query.zipCode);
+  getGeocode(zip: string): Promise<any> {
+    return this.mapsService.getGeocode(zip);
   }
 
-  @Post('searchNearby')
-  getNearbyZipCodes(
-    @Body() searchNearbyDTO: Record<string, any>,
-  ): Promise<any> {
+  @UseGuards(AuthGuard)
+  @Get('searchNearby')
+  async getNearbyZipCodes(@Request() req): Promise<any> {
+    const { location } = await this.getGeocode(req.query.zip);
+
     return this.mapsService.getNearbyZipCodes(
-      searchNearbyDTO.latitude,
-      searchNearbyDTO.longitude,
-      searchNearbyDTO.radius,
+      location.lat,
+      location.lng,
+      req.query.radius,
     );
   }
 }
