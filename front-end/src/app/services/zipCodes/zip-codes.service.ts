f54b5f932948d1e5cbdc0a87919863e5447dba86
LucasLinard <lucas.linard@outlook.com>
2023-11-26 18:20:53 -0300

    refactor: services folder structure

--- front-end/src/app/services/zipCodes/zip-codes.service.ts
+++ front-end/src/app/services/zipCodes/zip-codes.service.ts
@@ -10,7 +10,7 @@ export class ZipCodesService {
 
   async getSearchNearby(data: { zip: string; radius: number }) {
     let key =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJuYW1lIjoibWFyaWEiLCJpYXQiOjE3MDA4NzMyODAsImV4cCI6MTcwMDk1OTY4MH0.-pIoX2Qrjl5kKCWNrSXRHZgYreQIcNW3iiRgxv3jn5w';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJuYW1lIjoibWFyaWEiLCJpYXQiOjE3MDEwMjQwNDMsImV4cCI6MTcwMTExMDQ0M30.iLHJe9I71NitfERZkn8AjrEQd-DAxIiEyQiSSDHhrV4';
     return this.http.get(
       `http://localhost:3000/maps/searchNearby?zip=${data.zip}&radius=${
         data.radius * 1000
