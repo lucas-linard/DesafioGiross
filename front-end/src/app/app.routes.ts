7c9e7ded164e0d8e7abf2a4e674f43afd143f48f
LucasLinard <lucas.linard@outlook.com>
2023-11-26 23:20:15 -0300

    feature: added signup inside router

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/Home/home.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'signup', component: SignUpComponent, title: 'SignUp' },
 ];
